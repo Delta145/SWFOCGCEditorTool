713171494f8052b3091e8b41d9a50cb5be836499
Delta145 <sawin.georgij@yandex.ru>
2020-05-24 01:31:05 +0300

    correctly saving files if they are contain directories in filename

--- src/main/java/ru/ifmo/swfoc/io/XMLCampaignWriter.java
+++ src/main/java/ru/ifmo/swfoc/io/XMLCampaignWriter.java
@@ -8,14 +8,16 @@ import javax.xml.bind.Marshaller;
 import java.io.File;
 
 public class XMLCampaignWriter {
+    private final File originalDirectory;
     private File directory;
     public XMLCampaignWriter(File file) {
-        directory = file;
+        originalDirectory = file;
     }
 
     public void writeCampaignFile(CampaignWrapper wrapper) {
+        String fileName = makeDirectories(wrapper.getFileName());
         try {
-            File file = new File(directory, wrapper.getFileName());
+            File file = new File(directory, fileName);
             JAXBContext jaxbContext = JAXBContext.newInstance(CampaignWrapper.class);
             Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
             jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
@@ -23,5 +25,17 @@ public class XMLCampaignWriter {
         } catch (JAXBException e) {
             e.printStackTrace();
         }
+    }
+    
+    private String makeDirectories(String path) {
+        String[] pathParts = path.split("\\\\");
+        directory = originalDirectory;
+        for (int i = 0; i < pathParts.length-1; i++) {
+            String pathPart = pathParts[i];
+            directory = new File(directory, pathPart);
+            if (!directory.exists())
+                directory.mkdir();
+        }
+        return pathParts[pathParts.length-1];
     }
 }
