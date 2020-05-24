f3820ac5014a51d0c6ceea6516f755b305658e0e
Delta145 <sawin.georgij@yandex.ru>
2020-05-24 16:24:22 +0300

    updated build for windows

--- src/main/java/ru/ifmo/swfoc/Main.java
+++ src/main/java/ru/ifmo/swfoc/Main.java
@@ -1,7 +1,20 @@
 package ru.ifmo.swfoc;
 
-public class Main {
-    public static void main(String[] args) {
+import ru.ifmo.swfoc.editor.EditorCore;
+import ru.ifmo.swfoc.editor.GameEntities;
+import ru.ifmo.swfoc.io.Config;
+import ru.max.swfoc.ui.Graphics;
+import ru.max.swfoc.ui.Window;
 
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Config config = new Config("settings.properties");
+        EditorCore editorCore = new EditorCore(config);
+
+        GameEntities gameEntitiesStore = editorCore.getGameEntitiesStore();
+        Window win = new Window(new Graphics(gameEntitiesStore));
+        win.run();
     }
 }
